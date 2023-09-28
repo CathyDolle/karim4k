82632cd77e465a2f860795db3b0926f0230072f9
Cathy <45395549+CathyDolle@users.noreply.github.com>
2023-09-28 12:24:22 +0200

    bluerash

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { Blurhash } from "react-blurhash";
 
 function Thumbnail(thumb) {
+    const[imgLoaded, setImgLoaded] = useState(false)
+
+    useEffect(() =>{
+        const img = new Image()
+        img.onload = () =>{
+            setImgLoaded(true)
+        }
+        img.src = thumb.name
+    }, [thumb.name])
+
     return (
         <div onClick={thumb.onClick} className="thumbnail">
-            <img className={thumb.class} src={thumb.name} alt="img"/>
+            <>
+                <div style={{ display: imgLoaded ? 'none' : 'inline'}}>
+                    <Blurhash
+                        hash="L1O|b2-;fQ-;_3fQfQfQfQfQfQfQ"
+                        width='100%'
+                        height='100%'
+                        resolutionX={32}
+                        resolutionY={32}
+                        punch={1}
+                    />
+                </div>
+                <img style={{ display: !imgLoaded ? 'none' : 'inline'}}className={thumb.class} src={thumb.name}alt="img"/>
+            </>
             <div className="infos">
                 <p>{thumb.title}</p>
                 <p>{thumb.number}</p>
